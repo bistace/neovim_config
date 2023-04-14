9b243dbfe4487001556f1f2f814239975704071f
Benjamin Istace <bistace@genoscope.cns.fr>
2023-04-14 09:11:40 +0200

    Colorscheme

---
+++ after/plugin/colors.lua
@@ -0,0 +1,10 @@
+function setupColors(color) 
+	color = color or "rose-pine"
+        vim.cmd.colorscheme(color)
+
+	-- Transparent background
+	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+end
+
+setupColors()
